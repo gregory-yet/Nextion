5ecbfe61380be8c56c6d5708cc2b106725c877cc
Grégory <mailperso.gregory@gmail.com>
2016-06-02 12:38:10 +0200

    Update write function, easier to use

--- client.js
+++ client.js
@@ -17,36 +17,39 @@ var port = new SerialPort('/dev/ttyAMA0', {
 function init(){
 	port.on('open', function() {
 		console.log('Port ouvert sur /dev/ttyAMA0 @ 9600 bds');
-		write(1, false, "0");
+		write.setPage(0);
 	});
 
 	port.on('data', function(byte){
 		var data = byte.toString('hex').match(/.{1,2}/g);
 		readUart(data);
 	});
-	
-	// write(1, false, "1"); to change the actual page to page 1
-	// write(2, "t0", "mytext"); to change the text of t0 cmp
 }
 
-function write(type, cmp, cmd){ // type, components (t0, b0...), value
-	switch(type){
-		case 0: // commandes générales
-			writeUart(cmd);
-			break;
-		case 1: // changement de page
-			writeUart('page '+cmd)
-			break;
-		case 2: // changement de texte
-			writeUart(cmp+'.txt="'+cmd+'"');
-			break;
-		case 3: // visibilité
-			writeUart('vis '+cmp+','+cmd);
-			break;
-		
-		// you can add here some useful command
+// write.uart("any command");
+// write.setPage(0);
+// write.setText("t0", "hey");
+// write.setVis("t0", true);
+// write.getPage(); return actual page on nextion (sendme command)
+
+var write = {
+	uart: function(cmd){
+		writeUart(cmd);
+	},
+	setPage: function(num){
+		writeUart('page '+num);
+	},
+	setText: function(cmp, txt){
+		writeUart(cmp+'.txt="'+txt+'"');
+	},
+	setVis: function(cmp, value){
+		var val = value ? "1":"0";
+		writeUart('vis '+cmp+','+val);
+	},
+	getPage: function(){
+		writeUart('sendme');
 	}
-}
+};
 
 function writeUart(cmd){
 	port.write(hex(cmd));
